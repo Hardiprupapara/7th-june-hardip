8ec8aecb86109df067fc7547c24c1c4048fe56d4
Your Name <avinashgajetra02@gmail.com>
2022-10-14 10:18:05 +0530

    psd

--- hardip/javascript/intro.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-//alert("hello")
-=======
-alert("hello")
->>>>>>> 4c8e99c579b6389b01b0fb9f4a9eb4100b4cfa1b
-document.write(2+10);
-document.body.style.backgroundColor="skyblue"
-console.log(document.body.style)
